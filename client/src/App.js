a43f348f2e09c862b1f0b15c2097a4f75ef9a7f6
Marco Campanelli <mcampanelli333@hotmail.com>
2023-03-29 13:11:33 -0400

    Routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,19 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import GlobalStyle from "./GlobalStyles";
 
 const App = () => {
-  return <>Hi</>;
+  return (
+    <>
+      <GlobalStyle />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<>Hi there friend</>} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 };
 
 export default App;
