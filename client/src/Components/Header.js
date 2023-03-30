735ca4ff1585cc031bf528040b31cdf64cabc686
Marco Campanelli <mcampanelli333@hotmail.com>
2023-03-30 15:59:49 -0400

    Header

--- client/src/Components/Header.js
+++ client/src/Components/Header.js
@@ -16,19 +16,21 @@ const Header = () => {
       <Image src={Banner} />
       <Demo>
         {currentUser ? (
-          <>
-            <div>Hello, {currentUser}</div>
-            <button
+          <UsernameContainer>
+            <Name>Welcome, {currentUser}</Name>
+            <Button
               onClick={() => {
                 window.localStorage.clear();
                 setCurrentUser(null);
               }}
             >
               Log out
-            </button>
-          </>
+            </Button>
+          </UsernameContainer>
         ) : (
-          <button onClick={() => navigate("/signin")}>Sign In</button>
+          <SigninContainer>
+            <Button onClick={() => navigate("/signin")}>Sign In</Button>
+          </SigninContainer>
         )}
       </Demo>
     </HeaderContainer>
@@ -37,11 +39,14 @@ const Header = () => {
 
 export default Header;
 
+// Styles the entire Header component, it's position is relative to allow for the image to overlap it.
 const HeaderContainer = styled.div`
   position: relative;
   height: 15vh;
+  line-height: 15vh;
 `;
 
+// The image is given a position of absolite and is placed on top of the page with the same height as the header.
 const Image = styled.img`
   position: absolute;
   top: 0;
@@ -51,6 +56,37 @@ const Image = styled.img`
   height: 15vh;
 `;
 
+// Anything else that I want to belong to the header that isn't the image needs to be in here and given a position of relative
 const Demo = styled.div`
   position: relative;
+`;
+
+// This will give space to the username and the button
+const UsernameContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 0 1rem;
+`;
+
+// This will style the username
+const Name = styled.div`
+  font-size: 1.5rem;
+  font-weight: bold;
+`;
+
+// This will style the signout/signin button
+const Button = styled.button`
+  margin: auto 0;
+  padding: 0.5rem;
+  border-radius: 5px;
+
+  &:hover {
+    color: white;
+    background-color: lightblue;
+  }
+`;
+
+const SigninContainer = styled.div`
+  padding: 0 1rem;
+  text-align: right;
 `;
