3b47890c4db427ef77657124ae3d90d792b59a71
Marco Campanelli <mcampanelli333@hotmail.com>
2023-04-02 12:32:10 -0400

    Fetch problem solved

--- client/src/Components/SpecificPokemon.js
+++ client/src/Components/SpecificPokemon.js
@@ -5,21 +5,52 @@ import styled from "styled-components";
 
 const SpecificPokemon = () => {
   const id = useParams();
+  const [species, setSpecies] = useState("");
   const [pokemon, setPokemon] = useState("");
 
   useEffect(() => {
+    fetch(`https://pokeapi.co/api/v2/pokemon-species/${id.pokemon}`)
+      .then((res) => res.json())
+      .then((resData) => {
+        setSpecies(resData);
+      });
+
     fetch(`https://pokeapi.co/api/v2/pokemon/${id.pokemon}`)
       .then((res) => res.json())
       .then((resData) => {
         setPokemon(resData);
       });
-  }, []);
+  }, [id]);
 
-  if (!pokemon) {
+  const testing = (name) => {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
+      .then((res) => res.json())
+      .then((resData) => setPokemon(resData));
+  };
+
+  if (!species) {
     return <>Loading...</>;
   }
 
-  return <>{pokemon.name}</>;
+  console.log(species);
+
+  return (
+    <div>
+      <h1>{species.name}</h1>
+      {species.varieties.length !== 1 ? (
+        species.varieties.map((type) => {
+          return (
+            <button onClick={() => testing(type.pokemon.name)}>
+              {type.pokemon.name}
+            </button>
+          );
+        })
+      ) : (
+        <></>
+      )}
+      {pokemon ? <div>{pokemon.name}</div> : <>Sorry</>}
+    </div>
+  );
 };
 
 export default SpecificPokemon;
