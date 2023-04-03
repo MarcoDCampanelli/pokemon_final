5cd0bd60b8ca96eeec0a5311a4ee205cb9169bc7
Marco Campanelli <mcampanelli333@hotmail.com>
2023-04-03 12:00:53 -0400

    ErrorPage

--- client/src/Components/ErrorPage.js
+++ client/src/Components/ErrorPage.js
@@ -1,5 +1,26 @@
+import cryingPikachu from "../assets/crying pikachu.gif";
+
+import styled from "styled-components";
+
 const ErrorPage = () => {
-  return <>Error</>;
+  return (
+    <Container>
+      <p>Sorry, we are unable to find what you are looking for...</p>
+      <Image src={cryingPikachu} />
+    </Container>
+  );
 };
 
 export default ErrorPage;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  margin: 3rem auto;
+`;
+
+const Image = styled.img`
+  margin: 1rem auto;
+  width: 40%;
+`;
