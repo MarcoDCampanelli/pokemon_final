5d12dd2f7274f48e80199fabfa0d3abb58cddccc
Marco Campanelli <mcampanelli333@hotmail.com>
2023-04-23 12:57:57 -0400

    update

--- client/public/service-worker.js
+++ client/public/service-worker.js
@@ -1,22 +1,22 @@
-// // This is used in order to make the app a PWA
+// This is used in order to make the app a PWA
 
-// // This will cache the information
-// self.addEventListener("install", (e) => {
-//   e.waitUntil(caches.open("static")).then((cache) => {
-//     return cache.addAll([
-//       "./",
-//       "./assets/pokeball192.png",
-//       "./assets/pokeball512.png",
-//     ]);
-//   });
-// });
+// This will cache the information
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open("static")).then((cache) => {
+    return cache.addAll([
+      "./",
+      "./assets/pokeball192.png",
+      "./assets/pokeball512.png",
+    ]);
+  });
+});
 
-// // This will fetch the info from the cache or online
-// self.addEventListener("fetch", (e) => {
-//   console.log(cache);
-//   e.respondWith(
-//     caches.map(e.request).then((response) => {
-//       return response || fetch(e.request);
-//     })
-//   );
-// });
+// This will fetch the info from the cache or online
+self.addEventListener("fetch", (e) => {
+  console.log(cache);
+  e.respondWith(
+    caches.map(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
