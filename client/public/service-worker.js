bb07f5c15bd5509fc82266882585391157a4eadd
Marco Campanelli <mcampanelli333@hotmail.com>
2023-04-24 15:45:50 -0400

    testing fix break

--- client/public/service-worker.js
+++ client/public/service-worker.js
@@ -1,31 +1,43 @@
 // This is used in order to make the app a PWA
 
 // staticCacheName is given a version so that we can change the version should we ever change the site since the install event only happens once and won't include new changes
-const staticCacheName = "site-static-v0";
-const assets = ["./", "./assets/pokeball192.png", "./assets/pokeball512.png"];
-// this will hold the fetch requests dont after the initial cache
-const dynamicCache = "site-dynamic-v1";
+// !Removing to see if the problem is fixed
+// const staticCacheName = "site-static-v0";
+// const assets = ["./", "./assets/pokeball192.png", "./assets/pokeball512.png"];
+// // this will hold the fetch requests dont after the initial cache
+// const dynamicCache = "site-dynamic-v1";
 
-// This will cache the information
-self.addEventListener("install", (e) => {
-  e.waitUntil(caches.open(staticCacheName)).then((cache) => {
-    return cache.addAll(assets);
-  });
-});
+// // This will cache the information
+// self.addEventListener("install", (e) => {
+//   e.waitUntil(caches.open(staticCacheName)).then((cache) => {
+//     return cache.addAll(assets);
+//   });
+// });
 
 // This will fetch the info from the cache or online
-self.addEventListener("fetch", (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => {
-      return (
-        response ||
-        fetch(e.request).then((fetchRes) => {
-          return caches.open(dynamicCache).then((cache) => {
-            cache.put(e.request.url, fetchRes.clone());
-            return fetchRes;
-          });
-        })
-      );
-    })
-  );
-});
+
+// !Causing problems with profile?
+// self.addEventListener("fetch", (e) => {
+//   e.respondWith(
+//     caches.match(e.request).then((response) => {
+//       return (
+//         response ||
+//         fetch(e.request).then((fetchRes) => {
+//           return caches.open(dynamicCache).then((cache) => {
+//             cache.put(e.request.url, fetchRes.clone());
+//             return fetchRes;
+//           });
+//         })
+//       );
+//     })
+//   );
+// });
+
+// !Simplified version:
+// self.addEventListener("fetch", (e) => {
+//   e.respondwith(
+//     caches.match(e.request).then((response) => {
+//       return response || fetch(e.request);
+//     })
+//   );
+// });
--- client/src/index.js
+++ client/src/index.js
@@ -4,18 +4,19 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { UserProvider } from "./Components/UserContext";
 
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker
-    .register("service-worker.js")
-    .then((registration) => {
-      console.log("SW Registered!");
-      console.log(registration);
-    })
-    .catch((error) => {
-      console.log("SW Registration failed");
-      console.log(error);
-    });
-}
+// !Removing to see if something gets fixed
+// if ("serviceWorker" in navigator) {
+//   navigator.serviceWorker
+//     .register("service-worker.js")
+//     .then((registration) => {
+//       console.log("SW Registered!");
+//       console.log(registration);
+//     })
+//     .catch((error) => {
+//       console.log("SW Registration failed");
+//       console.log(error);
+//     });
+// }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
