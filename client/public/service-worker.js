5319936a93004e4cdf56034ce471475f9f53fbed
Marco Campanelli <mcampanelli333@hotmail.com>
2023-04-25 21:06:54 -0400

    no pwa

--- client/public/service-worker.js
+++ client/public/service-worker.js
@@ -2,12 +2,12 @@
 
 // staticCacheName is given a version so that we can change the version should we ever change the site since the install event only happens once and won't include new changes
 // !Removing to see if the problem is fixed
-// const staticCacheName = "site-static-v1";
+// const staticCacheName = "site-static-v0";
 // const assets = ["./", "./assets/pokeball192.png", "./assets/pokeball512.png"];
-// // this will hold the fetch requests dont after the initial cache
-// const dynamicCache = "site-dynamic-v1";
+// // // this will hold the fetch requests dont after the initial cache
+// const dynamicCache = "site-dynamic-v0";
 
-// // This will cache the information
+// // // This will cache the information
 // self.addEventListener("install", (e) => {
 //   e.waitUntil(caches.open(staticCacheName)).then((cache) => {
 //     return cache.addAll(assets);
@@ -20,6 +20,7 @@
 // self.addEventListener("fetch", (e) => {
 //   e.respondWith(
 //     caches.match(e.request).then((response) => {
+//       console.log("error", response);
 //       return (
 //         response ||
 //         fetch(e.request).then((fetchRes) => {
