94c8a8ff39082f20efbb0a329d0ddef08d89f2b8
Marco Campanelli <mcampanelli333@hotmail.com>
2023-04-26 14:00:55 -0400

    PWA final

--- client/public/service-worker.js
+++ client/public/service-worker.js
@@ -1,15 +1,11 @@
 // This is used in order to make the app a PWA
 
 // staticCacheName is given a version so that we can change the version should we ever change the site since the install event only happens once and won't include new changes
-const staticCacheName = "site-static-v0";
+const staticCacheName = "site-static-v1";
 const assets = [
   "./",
   "./assets/pokeball192.png",
   "./assets/pokeball512.png",
-  "./assets/Banner.jpg",
-  "./assets/cryingpikachu.gif",
-  "./assets/Pokeball.webp",
-  "./assets/runningpikachu.gif",
   "https://fonts.googleapis.com/css2?family=Cabin:ital,wght@0,400;0,700;1,400&family=Poppins:ital,wght@0,400;0,700;1,400;1,600&display=swap",
 ];
 
